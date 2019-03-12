a88db9ebdadbfa36491f7c56bcaa21bf6d4e71a0
zhouan <e-zhouan@cninfo.com.cn>
2019-03-12 15:18:46 +0800

    优化项目结构

---
+++ src/main/java/com/tca/SpringApplicationBoot.java
@@ -0,0 +1,11 @@
+package com.tca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationBoot {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationBoot.class, args);
+	}
+}
--- src/main/java/com/tca/controller/HelloworldController.java
+++ src/main/java/com/tca/controller/HelloworldController.java
@@ -1,22 +1,21 @@
 package com.tca.controller;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
-@Controller
+@RestController
 public class HelloworldController {
 	
 	@RequestMapping("/")
-	@ResponseBody
 	public String hello() {
 		return "hello springboot";
 	}
 	
-	public static void main(String[] args) {
-		SpringApplication.run(HelloworldController.class, args);
+	@RequestMapping("/hot")
+	public String hotDeployment() {
+		return "hot deployment";
 	}
+	
 }
